c823560884b50806408f4a3b9b7173cedfc07a7e
SamHoltz <holtzman.samuel1@gmail.com>
2018-08-08 22:02:51 -0500

    battlecruiser operational

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,63 +1,75 @@
-// Variables
-var score = 0;
-var winningScore = generateScore();
-var wins = 0;
-var losses = 0;
 
-
-// Comes up with winning score
-function generateScore() {
-    return (Math.floor(Math.random() * (120 - 19 + 1) + 19));
-
-}
-
-// crystal values
-function crystalGenerator() {
-    return (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-
-}
-
-// incrementing score
-function scoreKeep() {
-    var numberGenerated = crystalGenerator();
-    score += numberGenerated;
-    document.getElementById("incrementingScore").innerHTML = score
-}
-
-// on loading:
 $(document).ready(function () {
-    document.getElementById("totalScore").innerHTML = winningScore
 
-    $(".button1").on("click", function () {
-        scoreKeep();
+    var score = 0;
+    var winningScore = generateScore();
+    var wins = 0;
+    var losses = 0;
+    var crystalVal1 = 0;
+    var crystalVal2 = 0;
+    var crystalVal3 = 0;
+    var crystalVal4 = 0;
+
+    crystalGenerator();
+
+    function generateScore() {
+        return (Math.floor(Math.random() * (120 - 19 + 1) + 19));
+
+    }
+
+    function crystalGenerator() {
+
+        crystalVal1 = generateCrystalValue();
+        crystalVal2 = generateCrystalValue();
+        crystalVal3 = generateCrystalValue();
+        crystalVal4 = generateCrystalValue();
+
+        $('.button1').data('num', crystalVal1);
+        $('.button2').data('num', crystalVal2);
+        $('.button3').data('num', crystalVal3);
+        $('.button4').data('num', crystalVal4);
+
+    }
+
+    function generateCrystalValue() {
+        return Math.floor(Math.random() * (12 - 1 + 1) + 1);
+    }
+
+    function scoreKeep(val) {
+        score += val;
+        $("#incrementingScore").text(score);
+    }
+
+    function checkScore() {
+        if (score === winningScore) {
+            wins += 1;
+            alert("You Win!");
+            $("#wins").text(wins);
+            reset();
+        } else if (score > winningScore) {
+            losses += 1;
+            alert("You lose!")
+            $("#losses").text(losses);
+            reset();
+        }
+    };
+
+    function reset() {
+        score = 0;
+        winningScore = generateScore();
+        crystalGenerator();
+        $("#totalScore").text(winningScore);
+        $("#incrementingScore").text(score);
+    }
+
+    $("#totalScore").text(winningScore);
+
+    $(".crystal").on("click", function () {
+        var cval = $(this).data('num');
+        console.log(cval)
+        scoreKeep(cval);
         checkScore();
 
     });
 
 });
-
-
-// Winning game
-function checkScore() {
-    if (score === winningScore) {
-        wins += 1;
-        alert("You Win!");
-        document.getElementById("wins").innerHTML = wins;
-        turnClock();
-    } else if (score > winningScore) {
-        losses += 1;
-        alert("You lose!")
-        document.getElementById("losses").innerHTML = losses;
-        turnClock();
-    }
-};
-
-// clears score, generates new score
-function turnClock() {
-    score = 0;
-    document.getElementById("incrementingScore").innerHTML = score
-
-    winningScore = generateScore();
-    document.getElementById("totalScore").innerHTML = winningScore
-};
-
